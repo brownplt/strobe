dd890883ef4e39e7af81e0381a51fc0efc242a44
Arjun Guha <arjun@cs.brown.edu>
2010-02-18 12:58:50 -0500

    expunged Easy_format

--- src/javascript/javaScript_pretty.ml
+++ src/javascript/javaScript_pretty.ml
@@ -1,37 +1,7 @@
-open Easy_format
 open JavaScript_syntax
 open Prelude
-
-
-let seq_list = { list with 
-                 space_after_separator = false; 
-                 space_after_opening = false;
-                 space_before_closing = false
-               }
-
-let text s = Atom (s, { atom_style = Some "atom" })
-
-
-let parensBy sep xs = List (("(", sep, ")", seq_list), xs)
-
-
-let brackets xs = List (("[", ",", "]", list), xs)
-
-let bracesBy sep xs = List (("{", sep, "}", list), xs)
-
-
-let sepBy s xs = 
-  List (("",s, "", seq_list), xs)
-
-let sepEndBy s e xs = List (("",s,e, seq_list), xs)
-
-let seq xs = List (("","","",seq_list),xs)
-
-let spaces xs = List ((""," ","",seq_list),xs)
-
-(* concatenate (no spaces) *)
-let (@@) x y = List (("","","",seq_list),[x;y])
-
+open Format
+open FormatExt
 
 let prefixOp (op : prefixOp) = match op with
     PrefixLNot -> text "!"
@@ -98,9 +68,15 @@ let assignOp (op : assignOp) = match op with
 
 let string s = text ("\"" ^ s ^ "\"") (* TODO: fix escapes *)
 
-
-let int n = text (string_of_int n)
-
+let rec commas (ps : printer list) = match ps with
+    [] -> []
+  | [p] -> [p]
+  | p1 :: p2 :: ps -> 
+      (fun fmt -> 
+         pp_open_vbox fmt 0 ;
+         p1 fmt;
+         pp_print_string fmt ",";
+         pp_close_box fmt ()) :: commas (p2 :: ps)
 
 let prop (p : prop) = match p with
     PropId x -> text x
@@ -110,43 +86,43 @@ let prop (p : prop) = match p with
 
 let rec varDecl decl = match decl with
     VarDeclNoInit (_,x) -> text x
-  | VarDecl (_,x,e) -> sepBy " " [text x; text "="; expr e]
+  | VarDecl (_,x,e) -> sep [text x; text "="; expr e]
 
 
 and caseClause clause = match clause with
-    CaseClause (_,e,s) -> sepBy " " [expr e; text ":"; stmt s]
-  | CaseDefault (_,s) -> sepBy " " [text "default:"; stmt s]
-
-
-and parens e = match e with
-    ParenExpr _ -> expr e
-  | _ -> List (("(", "", ")", list), [expr e])
-
+    CaseClause (_,e,s) -> sep [expr e; text ":"; stmt s]
+  | CaseDefault (_,s) -> sep [text "default:"; stmt s]
 
 and block s = match s with
     BlockStmt _ -> stmt s
-  | _ -> List (("{", "", "}", list), [stmt s])
+  | _ -> vert [ text "{"; nest (stmt s); text "}" ]
+
+and paren_exp e = match e with
+    ParenExpr _ -> expr e
+  | _ -> sep [ text "("; expr e; text ")" ]
 
 
 and for_init fi = match fi with
     NoForInit -> text ""
-  | VarForInit decls -> spaces [text "var"; sepBy "," (List.map varDecl decls)]
+  | VarForInit decls -> sep [ text "var"; 
+                              vert (commas (List.map varDecl decls)) ]
   | ExprForInit e -> expr e
 
 
 and for_in_init fii = match fii with
-    VarForInInit (_,x) -> spaces [text "var"; text x]
+    VarForInInit (_,x) -> sep [text "var"; text x]
   | NoVarForInInit (_,x) -> text x
 
 
 and lvalue lv = match lv with
     VarLValue (_,x) -> text x
-  | DotLValue (_,e,x) -> sepBy "" [expr e; text "."; text x]
-  | BracketLValue (_,e1,e2) -> seq [expr e1; brackets [expr e2]]
+  | DotLValue (_,e,x) -> sep [expr e; text "."; text x]
+  | BracketLValue (_,e1,e2) -> sep [expr e1; brackets [expr e2]]
 
 
 and catch clause = match clause with
-    CatchClause (_,x,s) -> seq [text "catch"; parensBy "" [text x]; block s]
+    CatchClause (_,x,s) -> 
+      vert [ sep [ text "catch"; parens [text x] ]; block s ]
 
 
 and expr e = match e with
@@ -159,85 +135,86 @@ and expr e = match e with
   | NullExpr _ -> text "null"
   | ArrayExpr (_,es) -> brackets (List.map expr es)
   | ObjectExpr (_,ps) -> 
-      let f (_, p, e) = sepBy "" [prop p; text ":"; expr e]
-      in bracesBy "," (List.map f ps)
+      let f (_, p, e) = sep [prop p; text ":"; expr e]
+      in vert [ text "{"; nest (vert (map f ps)); text "}" ]
   | ThisExpr _ -> text "this"
   | VarExpr (_,x) -> text x
-  | DotExpr (_,e,x) -> seq [expr e; text "."; text x]
-  | BracketExpr (_,e1,e2) -> sepBy "" [expr e1; text "["; expr e2; text "]"]
+  | DotExpr (_,e,x) -> sep [expr e; text "."; text x]
+  | BracketExpr (_,e1,e2) -> sep [expr e1; text "["; expr e2; text "]"]
   | NewExpr (_,constr,args) -> 
-      sepBy "" [text "new "; expr constr; parensBy "," (List.map expr args)]
-  | PrefixExpr (_,op,e) -> sepBy "" [prefixOp op; expr e]
+      sep [text "new "; expr constr; parens (commas (List.map expr args)) ]
+  | PrefixExpr (_,op,e) -> sep [prefixOp op; expr e]
   | UnaryAssignExpr (_, op, lv) -> 
       if prefix_unaryAssignOp op
-      then unaryAssignOp op @@ lvalue lv
-      else lvalue lv @@ unaryAssignOp op
+      then sep [ unaryAssignOp op; lvalue lv ]
+      else sep [ lvalue lv; unaryAssignOp op ]
   | InfixExpr (_,op,e1,e2) ->
-      spaces [expr e1; infixOp op; expr e2]
+      sep [expr e1; infixOp op; expr e2]
   | IfExpr (_,e1,e2,e3) ->
-      sepBy " " [expr e1; text "?"; expr e2; text ":"; expr e3]
-  | AssignExpr (_,op,lv,e) -> spaces [lvalue lv; assignOp op; expr e]
-  | ParenExpr (_,e) -> parensBy "" [expr e]
-  | ListExpr (_,e1,e2) -> sepBy "," [expr e1; expr e2]
+      sep [expr e1; text "?"; expr e2; text ":"; expr e3]
+  | AssignExpr (_,op,lv,e) -> sep [lvalue lv; assignOp op; expr e]
+  | ParenExpr (_,e) -> sep [ parens [ expr e ] ]
+  | ListExpr (_,e1,e2) -> sep (commas [expr e1; expr e2 ])
   | CallExpr (_,func,args) ->
-      (expr func) @@ (parensBy "," (List.map expr args))
+      sep [ expr func; parens (commas (map expr args)) ]
   | FuncExpr (_,args,body) ->
-      sepBy " " [text "function"; parensBy "," (List.map text args); stmt body]
+      vert [ sep [ text "function"; parens (commas (map text args)) ];
+             stmt body ]
   | NamedFuncExpr (_,name,args,body) ->
-      sepBy " " [text "function"; text name; parensBy "," (List.map text args);
-                 stmt body]
+      vert [ sep [ text "function"; text name; 
+                   parens (commas (map text args)) ];
+             stmt body ]
   | UndefinedExpr _ -> text ""
 
 and stmt s = match s with
-   BlockStmt (_,ss) -> bracesBy "" (List.map stmt ss)
+   BlockStmt (_,ss) -> 
+     vert [ text "{"; nest (vert (List.map stmt ss)); text "}" ]
   | EmptyStmt _ -> text ";"
-  | ExprStmt e -> sepEndBy "" ";" [expr e]
+  | ExprStmt e -> sep [ expr e; text ";" ]
   | IfStmt (_,e,s1,s2) ->
-      sepBy " " [text "if"; parens e; stmt s1; text "else"; stmt s2]
-  | IfSingleStmt (_,e,s1) -> sepBy " " [text "if"; parens e; stmt s1]
+      vert [ sep [ text "if"; paren_exp e ]; stmt s1; text "else"; stmt s2 ]
+  | IfSingleStmt (_,e,s1) -> vert [ sep [text "if"; paren_exp e ]; stmt s1 ]
   | SwitchStmt (_,e,clauses) ->
-      sepBy " " [text "switch"; parens e;
-                 bracesBy "" (List.map caseClause clauses)]
-  | WhileStmt (_,e,s) -> sepBy " " [text "while"; parens e; stmt s]
+      vert [ sep [ text "switch"; paren_exp e ];
+             braces (List.map caseClause clauses) ]
+  | WhileStmt (_,e,s) -> vert [ sep [ text "while"; paren_exp e ]; stmt s ]
   | DoWhileStmt (_,s,e) -> 
-      sepBy " " [text "do"; stmt s; text "while"; parens e]
+      sep [text "do"; stmt s; text "while"; paren_exp e]
   | BreakStmt _ -> text "break;"
   | BreakToStmt (_,x) -> text ("break " ^ x ^ ";") 
   | ContinueStmt _ -> text "continue;"
   | ContinueToStmt  (_,x) -> text ("continue " ^ x ^ ";")
-  | LabelledStmt (_,x,s) -> sepBy " " [text (x ^ ":"); stmt s]
+  | LabelledStmt (_,x,s) -> sep [text (x ^ ":"); stmt s]
   | ForInStmt (_,fii,e,s) ->
-      spaces [text "for"; parensBy " " [for_in_init fii; text "in "; expr e];
-              block s]
+      vert [ sep [ text "for"; parens [ for_in_init fii; text "in "; expr e] ];
+              block s ]
   | ForStmt (_,fi,e1,e2,s) ->
-     spaces [text "for"; parensBy "; " [for_init fi; expr e1; expr e2];
-             stmt s]
+     vert [ sep  [text "for"; parens [ for_init fi; expr e1; expr e2 ] ];
+            stmt s ]
   | TryStmt (_,body,catches,EmptyStmt _) ->
-      seq [text "try"; block body; seq (List.map catch catches)]
+      vert (text "try" :: block body :: (map catch catches))
   | TryStmt (_,body,catches,finally) ->
-      seq [text "try"; block body; seq (List.map catch catches); 
-           text "finally"; block finally]
-  | ThrowStmt (_,e) -> sepBy " " [text "throw"; expr e; text ";"]
-  | ReturnStmt (_,e) -> 
-      sepEndBy " " ";" [text "return"; expr e]
-  | WithStmt (e,s) -> sepBy " " [text "with"; parens e; stmt s]
+      vert [ text "try"; block body; sep (map catch catches); 
+             text "finally"; block finally ]
+  | ThrowStmt (_,e) -> sep [text "throw"; expr e; text ";"]
+  | ReturnStmt (_,e) ->  sep [ text "return"; nest (expr e); text ";" ]
+  | WithStmt (e,s) -> sep [text "with"; paren_exp e; stmt s]
   | VarDeclStmt (_,decls) ->
-      sepEndBy " " ";" [text "var"; sepBy "," (List.map varDecl decls)] 
+      sep [ text "var"; 
+            vert (commas (map varDecl decls));
+            text ";" ]
   | FuncStmt (_,name,args,body) ->
-      sepBy " " [text "function"; text name; parensBy "," (List.map text args);
+      sep [text "function"; text name; parens (intersperse (text ",") 
+                                                 (List.map text args));
                  block body]
 
 
 
 
-let render_expr e : string = 
-  Easy_format.Pretty.to_string (expr e)
+let render_expr e : string = to_string expr e
 
-let render_stmts ss : string =
-  Easy_format.Pretty.to_string (sepBy "\n" (List.map stmt ss))
+let render_stmts ss : string = to_string (fun ss -> vert (map stmt ss)) ss
 
-let render_prefixOp op =
-  Easy_format.Pretty.to_string (prefixOp op)
+let render_prefixOp op = to_string prefixOp op
 
-let render_infixOp op =
-  Easy_format.Pretty.to_string (infixOp op)
+let render_infixOp op = to_string infixOp op
