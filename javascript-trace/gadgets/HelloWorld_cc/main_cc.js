4da02f1b97158542f01ce1c2902c9eef29087237
unknown <Laxori@.(none)>
2010-02-18 22:33:40 -0500

    added fully working traced GG example

---
+++ javascript-trace/gadgets/HelloWorld_cc/main_cc.js
@@ -0,0 +1,8 @@
+var onTextClick = __typedjs(function  ()
+                            {
+                              view.alert(HELLO_WORLD);
+                              return 200;
+                            },
+                            undefined,
+                            "onTextClick",
+                            0);
