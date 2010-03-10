8be6bd4255927cbd63c44a131906f2234f6a8352
Arjun Guha <arjun@cs.brown.edu>
2010-03-10 14:34:47 -0500

    non-lambda bindings in environment

--- src/lambdajs/lambdajs_env.ml
+++ src/lambdajs/lambdajs_env.ml
@@ -4,12 +4,20 @@ open Lexing
 
 type parsed_env = (id * exp) list
 
+(* All names "x" in the environment are renamed to "[[x]]" *)
+let rename_env (env : parsed_env) : parsed_env  =
+  let mk_name x = "[[" ^ x ^ "]]" in
+  let ns = map fst2 env in
+  let rename_rhs exp = fold_left (fun e x -> rename x (mk_name x) e) exp ns in
+  let rename_bind (x, exp) = (mk_name x, rename_rhs exp) in
+    map rename_bind env
+
 let parse_env cin name : parsed_env =
   let lexbuf = Lexing.from_channel cin in
     try 
       (* Set the correct filename in lexbuf (for source-tracking). *)
       lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = name };
-      Lambdajs_parser.env Lambdajs_lexer.token lexbuf
+      rename_env (Lambdajs_parser.env Lambdajs_lexer.token lexbuf)
     with
       |  Failure "lexing: empty token" ->
            failwith (sprintf "lexical error at %s"
@@ -20,15 +28,14 @@ let parse_env cin name : parsed_env =
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p)))
 
+let is_lambda (_, e) = match e with
+  | ELambda _ -> true
+  | _ -> false
 
-(* All names "x" in the environment are renamed to "[[x]]" *)
-let rename_env (env : parsed_env) : parsed_env  =
-  let mk_name x = "[[" ^ x ^ "]]" in
-  let ns = map fst2 env in
-  let rename_rhs exp = fold_left (fun e x -> rename x (mk_name x) e) exp ns in
-  let rename_bind (x, exp) = (mk_name x, rename_rhs exp) in
-    map rename_bind env
-
-let enclose_in_env (env : parsed_env) (exp : exp) : exp =
-  let env' = rename_env env in
-    EFix ((dummy_pos, dummy_pos), env', exp)
+let rec enclose_in_env (env : parsed_env) (exp : exp) : exp =
+  match take_while is_lambda env with
+    | [], [] -> exp
+    | [],  (x, e) :: env' -> 
+        ELet ((dummy_pos, dummy_pos), x, e, enclose_in_env env' exp)
+    | fix_binds, env' ->
+        EFix ((dummy_pos, dummy_pos), fix_binds, enclose_in_env env' exp)
