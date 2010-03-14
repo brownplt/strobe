feea8bdcebf9f62bf1ac700ee5e2ee41ff61dc53
U-BROWN-736676250\Laxori <Laxori@brown-736676250.(none)>
2010-03-14 16:49:36 -0400

    tests added, work file removed

--- tests/typable/constr3.js
+++ tests/typable/constr3.js
@@ -1,1 +1,18 @@
-//HAHA
+//TODO: subtyping between objects and constructed objects =/.
+//
+//function Point(x, y) /*: constructor (Int * Int -> {x : Int, y : Int}) */ {
+//    this.x = x;
+//    this.y = y;
+//  };
+//
+////subtyping =) ?
+//function sumPoint(pt) /*: {y : Int, x : Int} -> Int */ {
+//  return pt.x + pt.y;
+//}
+//function sumPoint2(pt) /*: {x : Int, y : Int} -> Int */ {
+//  return pt.x + pt.y;
+//}
+//
+//var p = new Point(10, 20);
+//sumPoint(p);
+//sumPoint2(p);
