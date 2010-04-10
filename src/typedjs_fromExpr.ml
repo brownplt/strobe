0e63a70005a6d1d2e980f2579d9bb655d5b1e48b
Arjun Guha <arjun.guha@gmail.com>
2010-04-10 14:41:56 -0400

    empty array elements are Refs

--- src/typedjs_fromExpr.ml
+++ src/typedjs_fromExpr.ml
@@ -63,7 +63,7 @@ let rec exp (env : env) expr = match expr with
   | ConstExpr (a, c) -> EConst (a, c)
   | HintExpr (p, txt, ArrayExpr (p', [])) -> 
       begin match parse_annotation p txt with
-        | ATyp t -> EEmptyArray (p, t)
+        | ATyp t -> EEmptyArray (p, TRef t)
         | _ -> 
             raise (Not_well_formed (p, "expected the type of array elemtns" ))
       end
