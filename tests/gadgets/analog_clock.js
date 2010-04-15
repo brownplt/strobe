a95b7d2e1c45fb8b10b673a22fa12621d59c1e10
csaftoiu <Claudiu_Saftoiu@Brown.edu>
2010-04-15 15:02:12 -0400

    changed metronome to use undefined instead of false. also i accidentally commented out part of analog_clock a while ago. i uncommented it now, it has type error

--- tests/gadgets/analog_clock.js
+++ tests/gadgets/analog_clock.js
@@ -35,9 +35,9 @@ function _view_onpopout() /*:  -> Void */ {
 }
 
 function d() /*:  -> Void */ {
-  /*var a = new Date(undefined); // Claudiu : added undefined
+  var a = new Date(undefined); // Claudiu : added undefined
     if (_minimized) {
-        var b = /*:upcast String + Int *//*(a.getHours());
+        var b = /*:upcast String + Int */(a.getHours());
         if (b > 12) b = b - 12;
         if (b < 10) b = "0" + b;
         var c = a.getMinutes();
@@ -48,7 +48,7 @@ function d() /*:  -> Void */ {
         k(a);
     }
     var i = (61 - a.getSeconds()) * 1000;
-    setTimeout(d, i);*/
+    setTimeout(d, i);
 }
 
 function e() /*:  -> Void */ {
