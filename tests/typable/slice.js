558765904c7dc3a92df3dad7d9b46c0c025b8a1a
Arjun Guha <arjun.guha@gmail.com>
2010-07-14 17:19:48 -0700

    fixed off-by-one errors

--- tests/typable/slice.js
+++ tests/typable/slice.js
@@ -1,14 +1,14 @@
 function slice(arr, start, stop) 
   /*: forall a . Array<'a> * Int * Int + Undef -> Array<'a> */ {
-    var len =  /*: is Int */(arr.length);
-  if (typeof stop === "undefined") { stop = len; }
+    var len =  arr.length;
+  if (typeof stop === "undefined") { stop = len - 1; }
 
   if (start < 0 || stop > len || start > stop) {
     throw "Invalid arguments";
   }
 
   var r = /*: 'a */ [ ];
-  for (var i = 0; i < stop - start; i++) {
+  for (var i = 0; i <= stop - start; i++) {
       r[i] = arr[start + i];
   }
 
