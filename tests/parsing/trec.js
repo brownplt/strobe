ecf256778a1ec5be3aa383b3ee56264fd0e043e4
joseph <joe@cs.brown.edu>
2010-08-08 15:00:49 -0700

    These make more sense this way

--- tests/parsing/trec.js
+++ tests/parsing/trec.js
@@ -1,3 +1,3 @@
-(/*: cheat trec X . (Num + X) */ 4);
-(/*: cheat trec X . ({___nodes___: {}, #proto: Object.prototype, *: X}) */ "foo");
+(/*: cheat trec X . (Num + 'X) */ 4);
+(/*: cheat trec X . ({___nodes___: {}, #proto: Object.prototype, *: 'X}) */ "foo");
 
