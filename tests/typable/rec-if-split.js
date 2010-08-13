d59aff5a401efa5396eb0782fd5c8a1640713f75
joseph <joe@cs.brown.edu>
2010-08-13 08:57:24 -0700

    Trying to figure out how if-splitting TRecs will work.

---
+++ tests/typable/rec-if-split.js
@@ -0,0 +1,12 @@
+var n = /*: upcast trec X . Int + {left: Int, right: 'X, toString: -> Str} */ 5;
+var o = /*: upcast trec X . Int + {left: Int, right: 'X, toString: -> Str} */ 
+{left:4, right:n};
+
+var d = 0;
+
+if(typeof o === "object") {
+    d = o.left + 6;
+}
+else {
+    d = o + 7;
+}
