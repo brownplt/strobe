384c4b37ef1ff7e2cc30f05df6262eaec1209f57
joseph <joe@cs.brown.edu>
2010-08-15 15:14:10 -0700

    More to this test

--- tests/typable/ad-type.js
+++ tests/typable/ad-type.js
@@ -1,4 +1,11 @@
-function simple(val) /*: (trec X . Undef + Num + {#proto: Object, *:'X}) -> (trec X . Undef + Num + {#proto: Object, *:'X}) */ {
+function simple(val) 
+/*: (trec X . Undef + Num + {#proto: Object, *:'X}) -> 
+(trec X . Undef + Num + {#proto: Object, *:'X}) */ {
     return val.foo;
 }
 
+function harder(val) 
+/*: (trec X . Undef + Num + {fld: Str, #proto: Object, *:'X}) -> 
+Undef + Str */ {
+    return val.fld;
+}
