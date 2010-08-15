55fb1ce7b3499362a848e06d557e3f984880d933
joseph <joe@cs.brown.edu>
2010-08-15 15:21:08 -0700

    Switch this test to use a function rather than vars.

--- tests/typable/star_plus_rec.js
+++ tests/typable/star_plus_rec.js
@@ -1,10 +1,4 @@
-var o = 
-    /*: upcast trec X . ({_banned_: {toString: -> Str}, #proto: Object, *: 'X}) */ 
-{_banned_:{}};
-
-
-var n = /*: upcast trec X . ({_banned_: {toString: -> Str}, #proto: Object, *: 'X} + Int) */ 5;
-var o2 = 
-    /*: upcast trec X . ({_banned_: {toString: -> Str}, #proto: Object, *: 'X} + Int) */ 
-{_banned_:{}, safe_field: n};    
-
+var f = function(o) /*: (trec X . {_banned_: {toString: -> Str}, #proto: Object, *: 'X}) ->
+(trec X . {_banned_: {toString: -> Str}, #proto: Object, *: 'X}) */ {
+    return {_banned_:{}, safe_field: o};
+};
