354144ffc74d8f7b2c43ad70e4a5ba1f36070200
Arjun Guha <arjun@cs.brown.edu>
2010-08-15 11:12:04 -0700

    Joe, please confirm that the essence of this modified test is the same.
    The flow analysis (really, the CPS transformation) ignores /*: upcast */
    annotations. So, it determined the false branch was unreachable when o was
    defined as an object.
    
    For these kinds of tests, use function parameters.

--- tests/typable/rec-if-split.js
+++ tests/typable/rec-if-split.js
@@ -1,12 +1,13 @@
-var o = /*: upcast trec X . Int + {left: Int, right: 'X, toString: -> Str} */ 
-{left:4, right:
-/*: upcast trec X . Int + {left: Int, right: 'X, toString: -> Str} */ 5};
+var func  = function(o) 
+/*: (trec X . Int + {left: Int, right: 'X, toString: -> Str}) -> Undef */ {
 
-var d = 0;
+  var d = 0;
 
-if(typeof o === "object") {
+  if(typeof o === "object") {
     d = o.left + 6;
-}
-else {
+  }
+  else {
     d = o + 7;
-}
+  }
+
+};
