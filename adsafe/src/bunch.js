55610885e6f2d36dfdc7945bcd18e7a6399fff8f
joseph <joe@cs.brown.edu>
2010-08-16 21:35:44 -0700

    More bunch methods --- similar patterns to value: and getValue:

--- adsafe/src/bunch.js
+++ adsafe/src/bunch.js
@@ -68,3 +68,81 @@ function Bunch_value(value) /*: [Ad] Ad -> Ad */ {
     return this;
 }
 
+function Bunch_title(value) /*: [Ad] Ad -> Ad */ {
+    if (this === this.window) {
+        return error('ADsafe error.');
+    }
+    var b = this.___nodes___, 
+    i = 0, // added by Joe
+    node = /*: upcast Undef + ASNode */ undefined;
+    if (value instanceof Array) {
+        if (value.length !== b.length) {
+            return error('ADsafe: Array length: ' + b.length +
+                         '-' + value.length);
+        }
+        for (i = 0; i < b.length; i += 1) {
+            node = b[i];
+            if (/*: cheat Bool */ node.tagName) {
+                node.title = String((/*: cheat Array<Ad> */ value)[i]);
+            }
+        }
+    } else {
+        for (i = 0; i < b.length; i += 1) {
+            node = b[i];
+            if (/*: cheat Bool */ node.tagName) {
+                node.title = String(value);
+            }
+        }
+    }
+    return this;
+}
+
+function Bunch_getTitle() /*: [Ad] -> Ad */ {
+    var a = /*: Ad */ [], 
+    b = this.___nodes___, 
+    i = 0; // Added by Joe
+    for (i = 0; i < b.length; i += 1) {
+        a[i] = b[i].title;
+    }
+    return a.length === 1 ? a[0] : /*: cheat Ad */ a;
+}
+
+function Bunch_on(type, func) /*: [Ad] Ad * Ad -> Ad */ {
+    if (this === this.window || typeof type !== 'string' ||
+        typeof func !== 'function') {
+        return error();
+    }
+
+    var b = this.___nodes___, 
+    i = 0,// Added by Joe
+    node /*: upcast Undef + ASNode */, 
+    on /*: upcast Undef + */, ontype;
+    for (i = 0; i < b.length; i += 1) {
+        node = b[i];
+
+        // The change event does not propogate, so we must put the handler on the
+        // instance.
+
+        if (type === 'change') {
+            ontype = 'on' + type;  // WTF?!  I edit this to not be foolish
+            if (node['onchange'] !== dom_event) {
+                node['onchange'] = dom_event;
+            }
+        }
+
+        // Register an event. Put the function in a handler array, making one if it
+        // doesn't yet exist for this type on this node.
+
+        on = node['___ on ___'];
+        if (!on) {
+            on = {};
+            node['___ on ___'] = on;
+        }
+        if (on.hasOwnProperty(type)) {
+            on[type].push(func);
+        } else {
+            on[type] = [func];
+        }
+    }
+    return this;
+}
