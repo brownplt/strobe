a67165d4f409222ab607cee5a69804cf4c5cbe7a
joseph <joe@cs.brown.edu>
2010-11-30 18:00:26 -0500

    Another test of string sets---this one is like reject

---
+++ tests/typable/strset2.js
@@ -0,0 +1,15 @@
+function foo(reject, o, s)
+/*: ((${"__proto__", "constructor"} -> True) + 
+     ($^{"__proto__", "constructor"} -> False)) *
+    {__proto__: Bool, "constructor": Int, #proto: Null, *: Str, #code: Bot} *
+    Str 
+  -> Str + Undef
+     */
+{
+    if(reject(s)) {
+        return "";
+    }
+    else {
+        return o[s];
+    }
+}
