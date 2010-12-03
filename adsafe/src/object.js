cfca4e66d3f3318aabeb74dd05af4d3fb35f8f12
joseph <joe@cs.brown.edu>
2010-12-03 11:44:20 -0500

    Ignoring delete for now, reject is in prelude

--- adsafe/src/object.js
+++ adsafe/src/object.js
@@ -1,36 +1,23 @@
-//  The reject function enforces the restriction on get and put.
-//  It allows access only to objects and arrays. It does not allow use of
-//  the banned names, or names that are not strings or positive numbers,
-//  or strings that start or end with _ or strings that start with -.
-
-function reject(object, name) 
-/*: 'Ad * 'Ad -> Any */ 
-{
-    return typeof object !== 'object'  || /*: cheat Any */ (banned[name]) ||
-        ((typeof name !== 'number' || name < 0) &&
-         (typeof name !== 'string'  || /*: cheat Str */ name.charAt(0) === '_' ||
-          /*: cheat Str */ name.slice(-1) === '_'     || /*: cheat Str */ name.charAt(0) === '-'));
-}
-
 function ADSAFE_get(object, name) 
 /*: 'Ad * 'Ad -> 'Ad */ 
 {
-    if (!reject(object, name)) {
-        return /*: cheat 'Ad */ (object[/*: cheat Str */name]);
+    if (typeof object !== "object") { return error(); }
+    if (!reject_name(name)) {
+        return object[name];
     }
     return error();
 }
 
 //  ADSAFE.remove deletes a value from an object.
 
-function ADSAFE_remove (object, name) 
-/*: 'Ad * 'Ad -> 'Ad */
-{
-    if (!reject(object, name)) {
-        /*: cheat Bool */ (delete object[/*: cheat Str + Int */ name]);
-        return;
-    }
-    return error();
+//function ADSAFE_remove (object, name) 
+///*: 'Ad * 'Ad -> 'Ad */
+//{
+//    if (!reject(object, name)) {
+//        /*: cheat Bool */ (delete object[/*: cheat Str + Int */ name]);
+//        return;
+//    }
+//    return error();
 }
 
 //  ADSAFE.set stores a value in an object.
@@ -38,8 +25,9 @@ function ADSAFE_remove (object, name)
 function ADSAFE_set (object, name, value) 
 /*: 'Ad * 'Ad * 'Ad -> 'Ad */
 {
-    if (/* arguments.length === 3 && */ !reject(object, name)) {
-        /*: cheat 'Ad */ (object[/*: cheat Str */ name] = value);
+    if (typeof object !== "object") { return error(); }
+    if (!reject_name(name)) {
+        object[name] = value;
         return;
     }
     return error();
