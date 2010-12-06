68f465739a97a5fff1af36f222b32c44993fc2f6
joseph <joe@cs.brown.edu>
2010-12-06 11:06:09 -0500

    Make sure we can parse the new obj* style

--- tests/parsing/objstar.js
+++ tests/parsing/objstar.js
@@ -2,5 +2,6 @@
 
 (/*: cheat {a: { x: Num, #proto: Function, *: Undef, #code: Num -> Num}, x: Num} */ 4);
 
-(/*: cheat {x: Num, y: Num, #proto: Number, *: Null, #code: -> Undef} */ "zomg");
+(/*: cheat {x: Num, y: Num, #proto: Num, *: Null, #code: -> Undef} */ "zomg");
 
+(/*: cheat {*: Num, #proto: Object, #code: Bot; f: Int, "*": Bool} */ "whatev");
