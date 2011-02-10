64f98258201fe75009c56fbefe43dba64093fb0d
joseph <joe@cs.brown.edu>
2011-02-10 16:44:53 -0500

    fix tests

--- tests/typable/this1.js
+++ tests/typable/this1.js
@@ -1,8 +1,10 @@
-var f = function(a) /*: [{f: Int -> Str, x: Str}] Int -> Str */ {
-  return this.x;
-};
-
-var obj = {f: function(n) /*: Int -> Str */ { return "str"; }, 
-           x : /*: upcast Str */ "HI"};
-obj.f(obj);
+function foo() /*: -> Undef */ {
+    function f(a) /*: [{f: Int -> Str, x: Str}] Int -> Str */ {
+        return this.x;
+    }
+    
+    var obj = {f: function(n) /*: Int -> Str */ { return "str"; }, 
+               x : /*: upcast Str */ "HI"};
+    obj.f(4);
+}
 
