0a28a8a81c6cb59895767fe1b81a6d3a674930a3
Arjun Guha <arjun.guha@gmail.com>
2011-02-17 18:59:38 -0500

    type fsm, state, etc are abstract

--- src/RegLang/regLang.mli
+++ src/RegLang/regLang.mli
@@ -18,27 +18,8 @@ type label =
   | In of CharSet.t
   | NotIn of CharSet.t
 
-type state = 
-    | I of int
-    | II of (state * state)
 
-module State : sig
-  type t = state
-  val compare : t -> t -> int 
-end
-
-module StateMap : Map.S
-  with type key = State.t
-
-module StateMapExt : MapExt.S
-  with type key = State.t
-  and type +'a t = 'a StateMap.t
-
-type fsm = {
-  edges: (label * state) list StateMap.t;
-  start: state;
-  accept: state;
-}
+type fsm
 
 val nfa_of_regex : regex -> fsm
 val intersect : fsm -> fsm -> fsm
