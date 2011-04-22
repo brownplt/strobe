be1dd5451218d8a93685fede939c69ce73b79134
joseph <joe@cs.brown.edu>
2011-04-22 09:53:56 -0400

    better parsing makes this example work better

--- tests/typable/obj-subtype5.js
+++ tests/typable/obj-subtype5.js
@@ -1,5 +1,5 @@
 function foo(o) /*: 
-    #{*: Str; /(a*)/: Bool, /b(b*)/: Bool}
-->  #{*: Bool + Str; /(a*|b)/: Int + Bool} */ {
+    #{*: Str, /(a*)/: Bool, /b(b*)/: Bool}
+->  #{*: Bool + Str, /(a*|b)/: Int + Bool} */ {
     return o;
 }
