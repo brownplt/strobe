f9a78a112eeb0bc600a0b41acd1490f91af30256
Arjun Guha <arjun@cs.brown.edu>
2011-06-18 13:18:06 -0700

    test script

--- tests/strobe-typable/test.sh
+++ tests/strobe-typable/test.sh
@@ -3,7 +3,7 @@
 P=`dirname $0`
 FILE=$1
 
-RESULT=`$P/../../tc -env $FILE.env -sb $FILE -noflows 2>&1`
+RESULT=`$P/../../tc -env $FILE.env -sb $FILE -noflows`
 if [ $? -ne 0 ]; then
   echo -e "$FILE: aborted with exit code $?; output was:\n$RESULT\n"
 fi
